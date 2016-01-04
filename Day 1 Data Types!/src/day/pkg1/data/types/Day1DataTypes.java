9fcf8a6eb7e6035d73f06d4f8b84f346c1722504
Md. Rana Mahmud <ranamd0@gmail.com>
2016-01-04 12:12:08 +0600

    Day 1 Data Type solved

---
+++ Day 1 Data Types!/src/day/pkg1/data/types/Day1DataTypes.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package day.pkg1.data.types;
+
+/**
+ *
+ * @author rana
+ */
+public class Day1DataTypes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        System.out.println("Primitive : double");
+        System.out.println("Primitive : char");
+        System.out.println("Primitive : boolean");
+        System.out.println("Primitive : int");
+        System.out.println("Referenced : String");
+        System.out.println("Primitive : boolean");
+        System.out.println("Primitive : double");
+        System.out.println("Primitive : char");
+        System.out.println("Referenced : String");
+
+    }
+
+}
